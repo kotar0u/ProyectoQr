6ed7a67ed30e6ba21c35894aeef178fb12e8bf6b
kotar0u <134973013+kotar0u@users.noreply.github.com>
2024-10-15 16:45:54 -0300

    Revert "asd"
    This reverts commit 0eb2a00a37b96648285c3fb2de41a667a234da22.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,13 @@ const routes: Routes = [
   },
   {
     path: 'home',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    //canActivate: [AuthenticatorService]
+    canActivate: [AuthenticatorService]
   },
   {
     path: 'login',
